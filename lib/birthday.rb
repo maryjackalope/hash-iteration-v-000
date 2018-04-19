8940ebf2ed51eec67b060f3e8f3914eca148bbd7
Mary Jack <maryjackinthebox@gmail.com>
2018-04-19 20:25:41 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,7 +16,9 @@ end
 winner
 
 def happy_birthday(birthday_kids)
-  
+   birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
