ff597436f1c67ce524358789bf070553db07d82d
Mary Jack <maryjackinthebox@gmail.com>
2018-04-19 20:24:32 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,15 @@
  }
 
 def select_winner
-  
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
+
+winner
+
 def happy_birthday(birthday_kids)
   
 end
