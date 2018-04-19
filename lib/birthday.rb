9a187b6e83d875ba33cdcb3b08c0a7bfa304c0c9
Mary Jack <maryjackinthebox@gmail.com>
2018-04-19 20:28:54 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,24 +4,6 @@
    "Amanda" => 27
  }
 
-passengers = {
-suite_a: "Amanda Presley",
-suite_b: "Seymour Hoffman",
-suite_c: "Alfred Tennyson",
-suite_d: "Charlie Chaplin",
-suite_e: "Crumpet the Elf"
-}
-
-def select_winner
-  winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end #if
-  end #do
-end
-
-winner
 
 def happy_birthday(birthday_kids)
    birthday_kids.each do |kids_name, age|
