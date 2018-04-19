d11fa57469dfe96204bbda6f6c406e5c58c753f1
Mary Jack <maryjackinthebox@gmail.com>
2018-04-19 20:27:38 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,6 +4,14 @@
    "Amanda" => 27
  }
 
+passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+}
+
 def select_winner
   winner = ""
 passengers.each do |suite, name|
